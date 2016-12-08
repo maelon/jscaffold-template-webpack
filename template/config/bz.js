feec238a75d164a4e005a9b1ac7c07470402c364
maelon <maelon.j@gmail.com>
2016-12-08 18:45:34 +0800

    add cache

--- template/config/bz.js
+++ template/config/bz.js
@@ -1,26 +1,28 @@
 !function() {
-    var xhr;
-    if(window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
-    } else {
-        throw new Error('unsupport XMLHttpRequest');
-    }
+    var ajax_get = function(url, successback, errorback) {
+        var xhr;
+        if(window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else {
+            throw new Error('unsupport XMLHttpRequest');
+        }
 
-    if(xhr) {
-        xhr.responseType = 'text';
-        xhr.timeout = 10000;
-        xhr.onreadystatechange = function(e) {
-            if(xhr.readyState === 4) {
-                if(xhr.status === 200) {
-                    success(xhr.responseText);
-                } else {
-                    fail(xhr.responseText);
+        if(xhr) {
+            xhr.responseType = 'text';
+            xhr.timeout = 10000;
+            xhr.onreadystatechange = function(e) {
+                if(xhr.readyState === 4) {
+                    if(xhr.status === 200) {
+                        successback(xhr.responseText);
+                    } else {
+                        errorback(xhr.responseText);
+                    }
                 }
             }
+            xhr.open('get', url, true);
+            xhr.send();
         }
-        xhr.open('get', 'vinfo.json?rand=' + Math.random().toString().slice(2), true);
-        xhr.send();
-    }
+    };
 
     var detectOS = function() {
         var ua = window.navigator.userAgent.toLowerCase();
@@ -50,9 +52,29 @@
             }
             s = document.createElement('script');
             s.setAttribute('type', 'text/javascript');
-            s.setAttribute('src', [version, n, h].join('/') + '.' + t);
-            document.body.appendChild(s);
-            s.onload = loadedHandler;
+            if(vinfo.cache) {
+                if(hasCache(n, h)) {
+                    s.innerHTML = getCache(n, h)['content'];
+                    document.body.appendChild(s);
+                    loadedHandler();
+                } else {
+                    ajax_get([version, n, h].join('/') + '.' + t, function(js) {
+                        var n = vinfo.moduleList[loadidx]['name'];
+                        var h = vinfo.moduleList[loadidx]['hash'];
+                        setCache(n, h, js);
+                        s.appendChild(document.createTextNode(js));
+                        loadidx += 1;
+                        insertJS();
+                    }, function(err) {
+                        throw new Error('loadm module '  + n + ' failed');
+                    });
+                    document.body.appendChild(s);
+                }
+            } else {
+                s.setAttribute('src', [version, n, h].join('/') + '.' + t);
+                document.body.appendChild(s);
+                s.onload = loadedHandler;
+            }
         }
     };
 
@@ -63,10 +85,86 @@
 
     var success = function(result) {
         vinfo = JSON.parse(result);
+        if(vinfo.cache) {
+            setCache('vinfo', new Date().toLocaleString(), JSON.stringify(vinfo));
+        } else {
+            clearCache();
+        }
         insertJS();
     };
 
     var fail = function(err) {
-        throw new Error(err);
+        if(hasCache('vinfo')) {
+            vinfo = JSON.parse(getCache('vinfo')['content']);
+            insertJS();
+        }
     };
+
+    ajax_get('vinfo.json?rand=' + Math.random().toString().slice(2), success, fail);
+
+    var hasCache = function(modname, modversion) {
+        if(window.localStorage && typeof modname === 'string') {
+            var mod = window.localStorage.getItem('paiband_cache_' + modname);
+            if(mod && modversion) {
+                mod = JSON.parse(mod);
+                return mod['version'] === modversion;
+            } else {
+                return !!mod;
+            }
+        }
+        return false;
+    };
+
+    var setCache = function(modname, modversion, modcontent) {
+        if(window.localStorage && typeof modname === 'string' && typeof modversion === 'string' && typeof modcontent === 'string') {
+            try {
+                window.localStorage.setItem('paiband_cache_' + modname, JSON.stringify({
+                    version: modversion,
+                    content: modcontent
+                }))
+            } catch(e) {
+            }
+        }
+    };
+
+    var getCache = function(modname, modversion) {
+        if(window.localStorage && typeof modname === 'string') {
+            var mod = window.localStorage.getItem('paiband_cache_' + modname);
+            if(mod) {
+                mod = JSON.parse(mod);
+                if(modversion) {
+                    if(mod['version'] === modversion) {
+                        return mod;
+                    }
+                } else {
+                    return mod;
+                }
+            }
+        }
+        return null;
+    };
+
+    var removeCache = function(modname, modversion) {
+        if(window.localStorage) {
+            if(hasCache(modname, modversion)) {
+                try {
+                    window.localStorage.removeItem(modname);
+                } catch(e) {
+                }
+            }
+        }
+    };
+
+    var clearCache = function() {
+        if(window.localStorage) {
+            var len = window.localStorage.length;
+            var i = 0, key;
+            for(var i = 0; i < len; i++) {
+                key = window.localStorage.key(i);
+                if(key.indexOf('paiband_cache_') !== -1) {
+                    removeCache(key);
+                }
+            }
+        }
+    }
 }();
